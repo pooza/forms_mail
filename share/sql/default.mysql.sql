51fa521c57bdbec74546618b72972580faa069ca
pooza <pooza@6bc21610-e001-0410-9273-fe8b54c00d2d>
2011-01-18 04:20:44 +0000

    git-svn-id: http://svn.b-shock.co.jp/commons/forms-mail/trunk@4510 6bc21610-e001-0410-9273-fe8b54c00d2d

--- share/sql/default.mysql.sql
+++ share/sql/default.mysql.sql
@@ -14,6 +14,120 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `article`
+--
+
+DROP TABLE IF EXISTS `article`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `article` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `connection_id` smallint(5) unsigned NOT NULL,
+  `body` text,
+  `criteria` text,
+  `create_date` datetime NOT NULL,
+  `update_date` datetime NOT NULL,
+  `sent_date` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `connection_id` (`connection_id`),
+  CONSTRAINT `article_ibfk_1` FOREIGN KEY (`connection_id`) REFERENCES `connection` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `article`
+--
+
+LOCK TABLES `article` WRITE;
+/*!40000 ALTER TABLE `article` DISABLE KEYS */;
+/*!40000 ALTER TABLE `article` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `connection`
+--
+
+DROP TABLE IF EXISTS `connection`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `connection` (
+  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `url` tinytext NOT NULL,
+  `uid` varchar(32) DEFAULT NULL,
+  `password` varchar(128) DEFAULT NULL,
+  `create_date` datetime NOT NULL,
+  `update_date` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `connection`
+--
+
+LOCK TABLES `connection` WRITE;
+/*!40000 ALTER TABLE `connection` DISABLE KEYS */;
+/*!40000 ALTER TABLE `connection` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `mail_log`
+--
+
+DROP TABLE IF EXISTS `mail_log`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `mail_log` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `article_id` int(10) unsigned NOT NULL,
+  `email` varchar(64) NOT NULL,
+  `create_date` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `article_id` (`article_id`),
+  CONSTRAINT `mail_log_ibfk_1` FOREIGN KEY (`article_id`) REFERENCES `article` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `mail_log`
+--
+
+LOCK TABLES `mail_log` WRITE;
+/*!40000 ALTER TABLE `mail_log` DISABLE KEYS */;
+/*!40000 ALTER TABLE `mail_log` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `recipient`
+--
+
+DROP TABLE IF EXISTS `recipient`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `recipient` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `connection_id` smallint(5) unsigned NOT NULL,
+  `email` varchar(64) NOT NULL,
+  `status` varchar(8) NOT NULL DEFAULT 'active',
+  `create_date` datetime NOT NULL,
+  `update_date` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`,`connection_id`),
+  KEY `connection_id` (`connection_id`),
+  CONSTRAINT `recipient_ibfk_1` FOREIGN KEY (`connection_id`) REFERENCES `connection` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `recipient`
+--
+
+LOCK TABLES `recipient` WRITE;
+/*!40000 ALTER TABLE `recipient` DISABLE KEYS */;
+/*!40000 ALTER TABLE `recipient` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -24,4 +138,4 @@
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2011-01-17 22:52:53
+-- Dump completed on 2011-01-18 13:20:19
