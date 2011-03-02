ece240b43cceef99bfbd76a4cfc9654b6bb8df61
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2011-03-02 17:18:03 +0900

    refs #1903

---
+++ webapp/modules/ConsoleConnection/actions/ReceiveEmptyMailAction.class.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * ReceiveEmptyMailアクション
+ *
+ * @package jp.co.commons.forms.mail
+ * @subpackage ConsoleConnection
+ * @author 小石達也 <tkoishi@b-shock.co.jp>
+ */
+class ReceiveEmptyMailAction extends BSAction {
+	public function execute () {
+
+	}
+}
+
+/* vim:set tabstop=4: */
