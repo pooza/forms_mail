e195bc46b297dad17dc7a3a660a13f2e5730b0fe
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2011-03-04 12:01:21 +0900

    refs #1905

---
+++ bin/send-article.rb
@@ -0,0 +1,19 @@
+#!/usr/local/bin/ruby -Ku
+
+# 記事の送信
+#
+# @package jp.co.commons.forms.mail
+# @author 小石達也 <tkoishi@b-shock.co.jp>
+
+path = File.expand_path(__FILE__)
+while (File.ftype(path) == 'link')
+  path = File.expand_path(File.readlink(path))
+end
+ROOT_DIR = File.dirname(File.dirname(path))
+$LOAD_PATH.push(ROOT_DIR + '/lib/ruby')
+
+require 'carrot/batch_action'
+
+actions = BatchAction.new
+actions.register('ConsoleArticle', 'Send')
+actions.execute
