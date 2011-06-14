e55028ff94d0b942a9b6600887bb5dd02e05363d
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2011-06-14 16:50:15 +0900

    carrot 2.4.90

--- tests/carrot/export/BSCSVExporterTest.class.php
+++ tests/carrot/export/BSCSVExporterTest.class.php
@@ -3,9 +3,9 @@
  * @package org.carrot-framework
  * @author 小石達也 <tkoishi@b-shock.co.jp>
  */
-class BSExcelExporterTest extends BSTest {
+class BSCSVExporterTest extends BSTest {
 	public function execute () {
-		$this->assert('__construct', $exporter = new BSExcelExporter);
+		$this->assert('__construct', $exporter = new BSCSVExporter);
 		$exporter->addRecord(new BSArray(array(
 			'name' => 'pooza',
 			'point' => 100,
@@ -14,7 +14,7 @@ class BSExcelExporterTest extends BSTest {
 			'name' => 'ビーショック',
 			'point' => 900,
 		)));
-		$this->assert('getType', $exporter->getType() == 'application/vnd.ms-excel');
+		$this->assert('getType', $exporter->getType() == 'text/csv');
 		$this->assert('getContents', !BSString::isBlank($exporter->getContents()));
 		$exporter->getFile()->delete();
 	}
