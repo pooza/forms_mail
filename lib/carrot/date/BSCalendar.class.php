dc33b50b24a70458e091ed1a2f5e39c6c361a9c3
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2011-08-11 18:00:09 +0900

    2.4.114

--- lib/carrot/date/BSCalendar.class.php
+++ lib/carrot/date/BSCalendar.class.php
@@ -9,8 +9,7 @@
  *
  * @author 小石達也 <tkoishi@b-shock.co.jp>
  */
-class BSCalendar implements IteratorAggregate {
-	private $dates;
+class BSCalendar extends BSParameterHolder {
 	private $start;
 	private $end;
 
@@ -20,24 +19,22 @@ class BSCalendar implements IteratorAggregate {
 	 * @param BSDate $end 終了日
 	 */
 	public function __construct (BSDate $start, BSDate $end) {
-		if ($start->isPast($end)) {
-			$this->start = $start;
-			$this->end = $end;
-		} else {
-			$this->start = $end;
-			$this->end = $start;
+		if (!$start->isPast($end)) {
+			throw new BSDateException('期間が正しくありません。');
 		}
 
+		$this->start = $start;
+		$this->end = $end;
 		$date = clone $this->getStartDate();
 		while ($date->getTimestamp() <= $this->getEndDate()->getTimestamp()) {
 			$values = new BSArray($date);
 			$values['date'] = $date->format('Y-m-d');
-			if ($date->isToday()) {
-				$values['today'] = true;
-			}
+			$values['today'] = $date->isToday();
 			$values['holiday'] = $date->isHoliday();
 			$values['holiday_name'] = $date->getHolidayName();
-			$this->getDates()->setParameter($date->format('Y-m-d'), $values);
+			$values['weekday'] = $date->getWeekday();
+			$values['weekday_name'] = $date->getWeekdayName();
+			$this->params[$date->format('Y-m-d')] = $values;
 			$date['day'] = '+1';
 		}
 	}
@@ -63,144 +60,57 @@ class BSCalendar implements IteratorAggregate {
 	}
 
 	/**
-	 * 指定した日付の情報を返す
+	 * パラメータを返す
 	 *
 	 * @access public
-	 * @param BSDate $date 日付
-	 * @return mixed[] 情報
+	 * @param string $name パラメータ名
+	 * @return mixed パラメータ
 	 */
-	public function getDate (BSDate $date) {
-		$dateKey = $date->format('Y-m-d');
-		foreach ($this->getDates() as $key => $values) {
-			if ($key == $dateKey) {
-				return $values;
-			}
+	public function getParameter ($name) {
+		if ($name instanceof BSDate) {
+			$name = $name->format('Y-m-d');
 		}
+		return parent::getParameter($name);
 	}
 
 	/**
-	 * 指定した日付の情報を返す
-	 *
-	 * getDateのエイリアス
+	 * パラメータを設定
 	 *
 	 * @access public
-	 * @param BSDate $date 日付
-	 * @return mixed[] 情報
-	 * @final
-	 */
-	final public function getDay (BSDate $date) {
-		return $this->getDate($date);
-	}
-
-	/**
-	 * 全ての日付を返す
-	 *
-	 * @access public
-	 * @return mixed[][] 全ての日付の情報
-	 */
-	public function getDates () {
-		if (!$this->dates) {
-			$this->dates = new BSArray;
-		}
-		return $this->dates;
-	}
-
-	/**
-	 * 全ての日付を返す
-	 *
-	 * getDatesのエイリアス
-	 *
-	 * @access public
-	 * @return mixed[][] 全ての日付の情報
-	 * @final
-	 */
-	final public function getDays () {
-		return $this->getDates();
-	}
-
-	/**
-	 * カレンダーに値を書き込む
-	 *
-	 * @access public
-	 * @param BSDate $date 日付
-	 * @param string $name 値の名前
+	 * @param string $name パラメータ名
 	 * @param mixed $value 値
 	 */
-	public function setValue (BSDate $date, $name, $value) {
-		$key = $date->format('Y-m-d');
-		if ($this->dates[$key]) {
-			if (!$this->dates[$key][$name]) {
-				$this->dates[$key][$name] = new BSArray;
-			}
-			$this->dates[$key][$name][] = $value;
+	public function setParameter ($name, $value) {
+		if ($name instanceof BSDate) {
+			$name = $name->format('Y-m-d');
+		}
+		if ($this->hasParameter($name)) {
+			parent::setParameter($name, $value);
 		}
 	}
 
 	/**
-	 * カレンダーから値を削除
+	 * パラメータが存在するか？
 	 *
 	 * @access public
-	 * @param BSDate $date 日付
-	 * @param string $name 値の名前
-	 * @param mixed $value 値
+	 * @param string $name パラメータ名
+	 * @return boolean 存在すればTrue
 	 */
-	public function removeValue (BSDate $date, $name) {
-		$key = $date->format('Y-m-d');
-		if ($this->dates[$key]) {
-			if (!$this->dates[$key][$name]) {
-				$this->dates[$key][$name] = new BSArray;
-			}
-			$this->dates[$key][$name]->clear();
+	public function hasParameter ($name) {
+		if ($name instanceof BSDate) {
+			$name = $name->format('Y-m-d');
 		}
+		return parent::hasParameter($name);
 	}
 
 	/**
-	 * カレンダーに同種の値をまとめて書き込む
+	 * パラメータを削除
 	 *
 	 * @access public
-	 * @param string $name 値の名前
-	 * @param BSArray $values 日付ごとの値
+	 * @param string $name パラメータ名
 	 */
-	public function setValues ($name, BSArray $values) {
-		foreach ($values as $key => $value) {
-			$this->setValue(BSDate::create($key), $name, $value);
-		}
-	}
-
-	/**
-	 * 全ての日付を週ごとに区切って返す
-	 *
-	 * @access public
-	 * @return BSArray 全ての日付の情報
-	 */
-	public function getWeeks () {
-		$weeks = new BSArray;
-		$date = clone $this->getStartDate();
-		$date['day'] = '-' . ($date->format('N') - 1);
-		$end = clone $this->getEndDate();
-		$end['day'] = '+' . (7 - $end->format('N'));
-
-		while ($date->getTimestamp() <= $end->getTimestamp()) {
-			if ($date->format('N') == 1) {
-				$week = new BSArray;
-				$weeks[] = $week;
-			}
-			if (!$values = $this->getDate($date)) {
-				$values = new BSArray($date);
-				$values['disabled'] = true;
-			}
-			$week[] = $values;
-			$date['day'] = '+1';
-		}
-		return $weeks;
-	}
-
-	/**
-	 * @access public
-	 * @return BSIterator イテレータ
-	 */
-	public function getIterator () {
-		return new BSIterator($this->getDates());
+	public function removeParameter ($name) {
+		throw new BSDateException('カレンダーから日付を削除できません。');
 	}
 }
 
